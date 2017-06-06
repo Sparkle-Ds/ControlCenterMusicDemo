e0182266e5cc018ddbcd5ae278b4636ff531f3ea
Sparkle-Ds <chengrong@DS-deMacBook-Pro.local>
2017-06-06 13:58:17 +0800

    Initial Commit

---
+++ ControlCenterMusicDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ControlCenterMusicDemo
+//
+//  Created by DS on 2017/6/6.
+//  Copyright © 2017年 First Cloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
